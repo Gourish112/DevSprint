eb19a062e1092ec7e8a982cc8c48595e4e16bfec
Gourish Bhatia <122691806+Gourish112@users.noreply.github.com>
2025-08-16 20:22:12 +0530

    Update apiConnector.js

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -3,11 +3,17 @@ import axios from "axios";
 export const axiosInstance = axios.create({});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
-  return axiosInstance({
-    method: `${method}`,
-    url: `${url}`,
-    data: bodyData ? bodyData : null,
-    headers: headers ? headers : null,
-    params: params ? params : null,
-  });
+    // Add Authorization header if a token is provided
+    const finalHeaders = headers ? { ...headers } : {};
+    if (headers && headers.Authorization) {
+        finalHeaders.Authorization = headers.Authorization;
+    }
+
+    return axiosInstance({
+        method: `${method}`,
+        url: `${url}`,
+        data: bodyData ? bodyData : null,
+        headers: finalHeaders,
+        params: params ? params : null,
+    });
 };
