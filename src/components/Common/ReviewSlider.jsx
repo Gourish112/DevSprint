d57ddb8c23f8da607c6ed5843dc5dc3a864f4c4f
Gourish Bhatia <122691806+Gourish112@users.noreply.github.com>
2025-08-17 18:56:11 +0530

    Update ReviewSlider.jsx

--- src/components/Common/ReviewSlider.jsx
+++ src/components/Common/ReviewSlider.jsx
@@ -37,19 +37,24 @@ function ReviewSlider() {
 
   return (
     <div className="text-white">
-      <div className="my-[50px] h-[184px] max-w-maxContentTab lg:max-w-maxContent">
+      <div className="my-[50px] max-w-maxContentTab lg:max-w-maxContent">
         <Swiper
-          slidesPerView={4}
-          spaceBetween={25}
-          loop={true}
-          freeMode={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          modules={[FreeMode, Pagination, Autoplay]}
-          className="w-full "
-        >
+      spaceBetween={25}
+      loop={true}
+      freeMode={true}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      breakpoints={{
+        320: { slidesPerView: 1 },
+        640: { slidesPerView: 2 },
+        1024: { slidesPerView: 3 },
+        1280: { slidesPerView: 4 },
+      }}
+      modules={[FreeMode, Pagination, Autoplay]}
+      className="w-full"
+    >
           {reviews.map((review, i) => {
             return (
               <SwiperSlide key={i}>
